70efe356b49191434e1afc21b5c3061f17b14d41
Imali Susan <susanimali52@gmail.com>
2022-02-15 17:58:41 +0300

    Add view page for all items

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -18,5 +18,12 @@ BookInstanceSchema
   return '/catalog/bookinstance/' + this._id;
 });
 
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+});
+
+
 //Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
