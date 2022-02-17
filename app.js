48075f1a9656f0436c6a6a63f90f40dd4a6b363a
Imali Susan <susanimali52@gmail.com>
2022-02-17 11:21:05 +0300

    Deploy to heroku

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://imalisusan:Strath$8552@cluster0.7aij0.mongodb.net/local_library?retryWrites=true&w=majority';
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var mongoDB = 'mongodb+srv://imalisusan:Strath$8552@cluster0.a9azn.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
